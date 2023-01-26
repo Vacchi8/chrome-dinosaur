e0df99099ef6794631d8fabd5094687e1dd9e73f
Giorgio Vacchiotti <emaildiprova@gmail.com>
2023-01-26 22:02:06 +0100

    commented out the code in 'appearance.c' in doxygen style

--- appearance.c
+++ appearance.c
@@ -1,5 +1,20 @@
+/**
+	\file appearance.c
+	\author Farbod Ahmadian
+	\version 1.0
+	\date 2023-01-26
+*/
+
 #include <ncurses.h>
 
+
+/**
+	\fn void dinosaur1(int y, int x)
+	\brief print a shape of the dinosaur (stampa una forma del dinosauro)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void dinosaur1(int y, int x) {
 	mvprintw(y-4, x, "          0-0  ");
 	mvprintw(y-3, x, "        /(\\_/)");
@@ -8,6 +23,14 @@ void dinosaur1(int y, int x) {
 	mvprintw(y, x,   "      \\/");
 }
 
+
+/**
+	\fn void dinosaur2(int y, int x)
+	\brief print a shape of the dinosaur (stampa una forma del dinosauro)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void dinosaur2(int y, int x) {
 	mvprintw(y-4, x, "          0-0  ");
 	mvprintw(y-3, x, "        /(\\_/)");
@@ -16,6 +39,14 @@ void dinosaur2(int y, int x) {
 	mvprintw(y, x,   "      /\\");
 }
 
+
+/**
+	\fn void cactus1(int y, int x)
+	\brief print a shape of the cactus (stampa una forma del cactus)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void cactus1(int y, int x) {
 	mvprintw(y-5, x, "    _ ");
 	mvprintw(y-4, x, " _ ( )   ");
@@ -25,6 +56,13 @@ void cactus1(int y, int x) {
 	mvprintw(y, x, "   | |");
 }
 
+/**
+	\fn void cactus2(int y, int x)
+	\brief print a shape of the cactus (stampa una forma del cactus)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void cactus2(int y, int x) {
 	init_pair(5,COLOR_GREEN,COLOR_BLACK);
 	attron(COLOR_PAIR(5));
@@ -38,6 +76,14 @@ void cactus2(int y, int x) {
 	attroff(COLOR_PAIR(5));
 }
 
+
+/**
+	\fn void sun(int y, int x)
+	\brief print the shape of the sun (stampa la forma del sole)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void sun(int y, int x) {
 	attron(COLOR_PAIR(6));
 	mvprintw(y-8, x, "      ;   :   ;");
@@ -52,6 +98,13 @@ void sun(int y, int x) {
 	attroff(COLOR_PAIR(6));
 }
 
+/**
+	\fn void moon(int y, int x)
+	\brief print the shape of the moon (stampa la forma della luna)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void moon(int y, int x) {
 	attron(COLOR_PAIR(4));
 	mvprintw(y-8, x, "                      ");
@@ -66,6 +119,13 @@ void moon(int y, int x) {
 	attroff(COLOR_PAIR(4));
 }
 
+/**
+	\fn void showLoss(int y, int x)
+	\brief print the text "LOSS" in a prettier way, made up of characters (stampa la scritta "LOSS" in un modo piu' carino, fatto di caratteri)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void showLoss(int y, int x) {
 	mvprintw(y-6, x-15, "88");
 	mvprintw(y-5, x-15, "88");
@@ -79,6 +139,13 @@ void showLoss(int y, int x) {
 	mvprintw(y+4, x-15, "Or 'q' to exit from game.");
 }
 
+/**
+	\fn void showTrex(int y, int x)
+	\brief print the text "Trex" in a prettier way, made up of characters (stampa la scritta "TREX" in un modo piu' carino, fatto di caratteri)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void showTrex(int x, int y) {
 	mvprintw(x-7, y-25, "  ,d");
 	mvprintw(x-6, y-25, "  88");
@@ -89,7 +156,13 @@ void showTrex(int x, int y) {
 	mvprintw(x-1, y-25, "  \"Y888 88          `\"Ybbd8\"' 8P\'     `Y8");
 }
 
-
+/**
+	\fn void clearCactus1(int y, int x)
+	\brief cleans the cactus print when it goes off the screen and when the game is over (pulisce il disegno del cactus quando esce dallo schermo e quando la partita e' finita)
+	\param int y: y coordinate of the characters to be printed (coordinata y dei caratteri da stampare)
+	\param int x: x coordinate of the characters to be printed (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void clearCactus1(int y, int x) {
 	mvprintw(y-6, x+7, "        ");
 	mvprintw(y-5, x+8, "           ");
@@ -109,6 +182,13 @@ void clearCactus1(int y, int x) {
 	}
 }
 
+/**
+	\fn void cleardinosaurDown(int diY, int diX)
+	\brief cleans the dinosaur print when the game is over and he's not on jump (pulisce il disegno del cactus quando la partita e' finita e il dinosauro non e' in salto)
+	\param int y: diY coordinate of the characters to clean (coordinata y dei caratteri da stampare)
+	\param int x: diX coordinate of the characters to clean (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void cleardinosaurDown(int diY, int diX) {
 	mvprintw(diY+3, diX+10, "              ");
 	mvprintw(diY+4, diX+8, "              ");
@@ -117,6 +197,14 @@ void cleardinosaurDown(int diY, int diX) {
 	mvprintw(diY+7, diX, "                "); 
 }
 
+
+/**
+	\fn void cleardinosaurUp(int diY, int diX)
+	\brief cleans the dinosaur print when the game is over and he's on jump (pulisce il disegno del cactus quando la partita e' finita e il dinosauro e' in salto)
+	\param int y: diY coordinate of the characters to clean (coordinata y dei caratteri da stampare)
+	\param int x: diX coordinate of the characters to clean (coordinata x dei caratteri da stampare)
+	\return void function, no return (funzione void, nessun return)
+*/
 void cleardinosaurUp(int diY, int diX) {
     mvprintw(diY-7, diX+4, "         ");
 	mvprintw(diY-8, diX+1, "               ");
